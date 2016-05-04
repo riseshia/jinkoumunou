28bb84dac26391f19ec77c4582450b237e38ae2b
Shia <rise.shia@gmail.com>
2016-05-04 13:33:20 +0900

    chapter 5-1

--- responder.rb
+++ responder.rb
@@ -22,9 +22,20 @@ end
 class RandomResponder < Responder
   def initialize(name)
     super
-    @responses = %w[今日は寒いですね。 チョコ食べたいです。 昨日10円を拾いました。]
+    @phrases = []
+    open('dic/random.txt') do |f|
+      f.each do |line|
+        line.chomp!
+        @phrases.push(line) unless line.empty?
+      end
+    end
   end
-  def response(input)
-    @responses[rand(@responses.size)]
+
+  def select_random(array)
+    array[rand(array.size)]
+  end
+
+  def response(_input)
+    select_random(@phrases)
   end
 end
